ddc961c895021a09c6c13e5665224022284dd284
DerikGagnon <derik.gagnon@mymail.champlain.edu>
2019-02-20 14:19:44 -0500

    don't need knapsack class

--- code/main.cpp
+++ code/main.cpp
@@ -3,6 +3,8 @@
 
 int main() {
     std::string filename = "test.txt";
+    std::string tempName;
+    int tempValue = 0, tempWeight = 0;
     Item * itemArray;
     std::ifstream fin;
     fin.open(filename);
@@ -12,7 +14,10 @@ int main() {
     itemArray = new Item[numberOfItems];
 
     for (int i = 0; i < numberOfItems; i++ ) {
-        
+        fin >> tempName >> tempWeight >> tempValue;
+        itemArray[i].setName(tempName);
+        itemArray[i].setWeight(tempWeight);
+        itemArray[i].setValue(tempValue);
     }
 
     std::cin.get();
